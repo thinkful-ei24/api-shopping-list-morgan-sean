cdbca95737d6159eddae3d1fb7cea37423886de3
Sean Phelan <seanphelan97@gmail.com>
2018-08-29 09:54:48 -0600

    add createItem, update store methods

--- scripts/api.js
+++ scripts/api.js
@@ -1,11 +1,29 @@
+'use strict';
+
+/* globals $ */
+
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/morgan-sean';
+  const ITEMS_URL = `${BASE_URL}/items`;
 
   const getItems = function(callback) {
-    callback();
+    $.getJSON(ITEMS_URL, callback);
+  };
+
+  const createItem = function(name, callback) {
+    let newItem = { name };
+    let jsonData = JSON.stringify(newItem);
+    const data = {
+      method: 'POST',
+      contentType: 'application/json',
+      data: jsonData,
+      success: callback
+    };
+    $.ajax(ITEMS_URL, data);
   };
 
   return {
-    getItems
+    getItems,
+    createItem
   };
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
-/* global shoppingList, store */
+'use strict';
+
+/* global shoppingList, api, store, $ */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
-});
 
+  api.getItems((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+});
